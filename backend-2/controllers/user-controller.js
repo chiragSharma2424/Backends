ea30286e6d30547d01cdb0a31d1940d7bbb19780
chirag <sharmachirag242004@gmail.com>
2025-07-18 17:44:39 +0530

    login controller done

--- backend-2/controllers/user-controller.js
+++ backend-2/controllers/user-controller.js
@@ -1,11 +1,39 @@
+import userModel from "../models/user-model.js";
+import jwt from 'jsonwebtoken';
+
+
 const signup = async (req, res) => {
     try {
         const { firstName, lastName, email, password } = req.body;
-        if(! firstName || !lastName || email || password) {
+
+        if(! firstName || !lastName || !email || !password) {
             return res.status(400).json({
                 msg: "all fields are required"
             })
         }
+
+        const existingUser = await userModel.findOne({email});
+
+        if(existingUser) {
+            return res.status(200).json({
+                success: false,
+                msg: "user already exists"
+            })
+        }
+
+        const newUser = await userModel.create({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password
+        });
+
+        return res.status(200).json({
+            success: true,
+            msg: "user created successfully",
+            user: newUser
+        });
+
     } catch(err) {
         console.log(`error in signup controller ${err}`);
         return res.status(500).json({
@@ -16,6 +44,21 @@ const signup = async (req, res) => {
 };
 
 
+// login controller, jo email and pass lega and genrates a jwt token
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+    } catch(err) {
+        console.log(err);
+        return res.status(500).json({
+            success: false,
+            msg: "internal server error"
+        })
+    }
+}
+
 export {
-    signup
+    signup,
+    login
 }
