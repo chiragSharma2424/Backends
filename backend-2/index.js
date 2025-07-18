4ebf710f7c55142958f2208ae72f1f296d58b1ba
chirag <sharmachirag242004@gmail.com>
2025-07-18 15:35:31 +0530

    basic routing in express done

--- backend-2/index.js
+++ backend-2/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './db/db.js';
+import userRouter from './routes/user-routes.js';
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
@@ -12,7 +13,14 @@ app.get('/', (req, res) => {
     })
 });
 
+
+// all routes, all user routes are handled by user router
+app.use('/api/v1/users', userRouter);
+
+
+// a function jo database connect krega
 connectDB();
+
 
 app.listen(port, () => {
     console.log(`server started on port ${port}`);
