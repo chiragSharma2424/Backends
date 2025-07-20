06c68d946a2541191fdaecd512e7fffbf00d3611
chirag <sharmachirag242004@gmail.com>
2025-07-20 13:48:41 +0530

    login controller done

--- backend-2/controllers/admin-controller.js
+++ backend-2/controllers/admin-controller.js
@@ -20,3 +20,15 @@ export const signup = async (req, res) => {
         })
     }
 }
+
+export const login = async (req, res) => {
+    try {
+
+    } catch(err) {
+        console.log(`error in login controller ${err}`);
+        return res.status(500).json({
+            success: false,
+            msg: "internal server error"
+        })
+    }
+}
