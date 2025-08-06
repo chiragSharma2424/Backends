b066af8f5a114c9e5216181c1588b5c2930cdc13
chirag <sharmachirag242004@gmail.com>
2025-08-06 16:55:37 +0530

    sigup route using array

--- course-app-easy/index.js
+++ course-app-easy/index.js
@@ -1,17 +1,48 @@
+// in this backend we are not using database we use global arrays
+
 import express from 'express';
+import jwt from 'jsonwebtoken';
 const app = express();
 import cors from 'cors';
 const port = 3000;
+app.use(cors());
+app.use(express.json());
+const secretkey = 'secret24';
 
-let ADMINS = [];
-let USERS = [];
-let COURSE = [];
+
+const ADMINS = [];
+const USERS = [];
+const COURSE = [];
 
 app.get('/', (req, res) => {
     return res.json({
         msg: "api is working"
-    })
+    });
 });
+
+app.post('/admin/signup', (req, res) => {
+    const admin = req.body;
+
+    const existingAdmin = ADMINS.find((a) => {
+    return a.username === admin.username;
+     });
+     
+    if(existingAdmin) {
+        return res.status(400).json({
+            message: "Admin already exists"
+        })
+    } else {
+        ADMINS.push(admin);
+        console.log(ADMINS);
+        const token = jwt.sign(admin, secretkey, { expiresIn: '1h'});
+        return res.status(200).json({
+            message: "Admin created successfully",
+            token: token
+        })
+    }
+
+});
+
 
 app.listen(port, () => {
     console.log(`server is started on port ${port}`)
