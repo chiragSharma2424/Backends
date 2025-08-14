f23d8726261149b2db9b83982fc09841c684f7f3
chirag <sharmachirag242004@gmail.com>
2025-08-14 16:11:42 +0530

    models, db done

--- auth-2/backend/index.js
+++ auth-2/backend/index.js
@@ -1,13 +1,50 @@
 import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
-const port = 3000;
+app.use(express.json());
+app.use(cors());
+const port = process.env.PORT;
 
+
+// write function which connects to db for now we are using mongoDB compass
+function connectsToDB() {
+    mongoose.connect(process.env.MONGO_URL).then(() => {
+        console.log("database connected")
+    }).catch((err) => {
+        console.log(`error in database connection ${err}`)
+    })
+}
+
+
+
+
+// writing model for user
+const userSchema = new mongoose.Schema({
+    fullName: {type: String, required: true},
+    email: {type: String, required: true, lowercase: true, trim: true},
+    password: {type: String, required: true}
+}, {timestamps: true})
+
+const User = mongoose.model("user", userSchema);
+
+
+
+// testing endpoint
 app.get('/', (req, res) => {
-    return res.json({
-	 msg: "endpoint is working"
-    });
+   return res.status(200).json({
+      message: "api endpoint is working"
+   })
 });
 
+
+
+
+// function call jo database connect krega
+connectsToDB();
 app.listen(port, () => {
     console.log(`server started on ${port}`);
 });
